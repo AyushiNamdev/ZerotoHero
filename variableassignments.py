835ef4abe55b27b0b919b29f9f5608ec29db0768
AyushiNamdev <ayushinamdev2@gmail.com>
2022-03-01 09:40:57 -0500

    slicing

---
+++ variableassignments.py
@@ -0,0 +1,14 @@
+
+mystring = 'abcdefghijk'
+print(mystring[:3])
+
+print(mystring[3:6])
+
+print(mystring[1:3])
+
+print(mystring[::])
+print(mystring[::2])
+print(mystring[::3])
+print(mystring[2:7:2])
+print(mystring[::-1])
+
